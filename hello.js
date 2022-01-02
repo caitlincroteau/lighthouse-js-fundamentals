5be041c4f7d47381f58bc63bcbd775d43105cc34
Caitlin <cjcroteau@gmail.com>
2022-01-02 01:37:05 +0000

    adding hello file

---
+++ hello.js
@@ -0,0 +1,19 @@
+/*
+const sayHello  = function () {
+  console.log("Hello, world");
+}
+*/
+
+
+
+const sayHello = function (name) {
+  console.log("Hello, " + name);
+}
+
+
+
+
+sayHello("Caitlin");
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
