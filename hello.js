017384743a54588eda9adbec647a59b346cd52f0
Caitlin <cjcroteau@gmail.com>
2022-01-02 01:47:58 +0000

    modified hello file and added loopy-lighthouse file

--- hello.js
+++ hello.js
@@ -2,18 +2,26 @@
 const sayHello  = function () {
   console.log("Hello, world");
 }
-*/
-
-
 
 const sayHello = function (name) {
   console.log("Hello, " + name);
 }
 
-
-
-
 sayHello("Caitlin");
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+*/
+
+
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); 
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('James');
+
+//console.log(greeting);
